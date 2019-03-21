b0119b88eaca550a6ec35cc96baa27c6d274ea28
guang <ghu1@student.unimelb.edu.au>
2019-03-21 21:44:50 +1100

    fixed multiple issues with camera and armor detection when running on real robot; we need to tie the camera more firmly to the gimbal

--- roborts_detection/armor_detection/gimbal_control.cpp
+++ roborts_detection/armor_detection/gimbal_control.cpp
@@ -40,7 +40,7 @@ float GimbalContrl::BulletModel(float x, float v, float angle) { //x:m,v:m/s,ang
   return y;
 }
 
-//x:distance , y: height
+//x:distance (m) , y: height (m)
 float GimbalContrl::GetPitch(float x, float y, float v) {
   float y_temp, y_actual, dy;
   float a;
@@ -60,11 +60,12 @@ float GimbalContrl::GetPitch(float x, float y, float v) {
 
 }
 
-void GimbalContrl::Transform(cv::Point3f &postion, float &pitch, float &yaw) {
+void GimbalContrl::Transform(cv::Point3f &position, float &pitch, float &yaw) {
   pitch =
-      -GetPitch((postion.z + offset_.z) / 100, -(postion.y + offset_.y) / 100, 15) + (float)(offset_pitch_ * 3.1415926535 / 180);
+      -GetPitch((position.z + offset_.z) / 1000, -(position.y + offset_.y) / 1000, 15) + (float)(offset_pitch_ * 3.1415926535 / 180);
   //yaw positive direction :anticlockwise
-  yaw = -(float) (atan2(postion.x + offset_.x, postion.z + offset_.z)) + (float)(offset_yaw_ * 3.1415926535 / 180);
+  ROS_INFO("distance is %f; height is %f, target position x = %f, y = %f", (position.z + offset_.z) / 100, -(position.y + offset_.y) / 100, position.z, position.y);
+  yaw = -(float) (atan2(position.x + offset_.x, position.z + offset_.z)) + (float)(offset_yaw_ * 3.1415926535 / 180);
 }
 
 } // roborts_detection
